4af921e82b728413b9d5d2e1b3e563f511eb6818
mrpowersnichols <gpowers@nicholsschool.org>
2026-08-21 15:46:10 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,12 @@ import {
   getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged,
 } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import {
+  getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged,
+} from "firebase/auth";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,10 +22,6 @@ const firebaseConfig = {
   appId: "1:324877421887:web:45ba3fe97d2c9c5368d78a",
   measurementId: "G-D0H78LZLFB"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
